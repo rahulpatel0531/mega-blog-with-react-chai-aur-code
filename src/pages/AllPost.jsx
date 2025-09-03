58ee1befd0433a2662bdb0241670f23c68a648df
Rahul Patel <p.rahul0531@gmail.com>
2025-09-03 20:14:05 +0530

    fix postCard prop  issue

--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,33 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import appwriteService from '../appwrite/config'
-import { PostCard, Container } from '../components'
-
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/config";
+import { PostCard, Container } from "../components";
 
 function AllPost() {
-  const [posts, setPosts]  =  useState([]);
+  const [posts, setPosts] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     appwriteService.getPosts([]).then((posts) => {
-      if(posts) {
-        setPosts(posts.documents)
+      if (posts) {
+        setPosts(posts.documents);
       }
-    })
-
+    });
   }, []);
   return (
-    <div className='w-full py-8'>
-      <Container >
-        <div className='flex flex-wrap'>
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
           {posts.map((post) => (
-            <div className='p-2 w-1/4' key={post.$id} >
-              <PostCard post={post} />
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard {...post} />
             </div>
           ))}
         </div>
       </Container>
-      
     </div>
-  )
+  );
 }
 
-export default AllPost
+export default AllPost;
